9e11f230499ed131771014cc39ac38c04ee4528e
jku20 <jku-ben20@stuy.edu>
2021-01-20 14:44:58 -0500

    chipping away at the simulator

--- simulator.c
+++ simulator.c
@@ -15,6 +15,54 @@ int main() {
 
     //brain is on the fizzle. this is where we will finish taking input and setting up shared memeory which the renderer can read from and render the entities in there.
 
+    char running = 1;
+    int gamestate = 0;
+    char in_buff[MAX_KEYS_AT_ONCE];
+
+    while(running) {
+        //readed keycodes from 
+        //ASSUMES ONLY ONE SEQUENCE OF KEYS IS READ AT ONCE 
+        read(fd[1], in_buff, sizeof(in_buff));
+
+        char * input_pointer = in_buff;
+        int keycode = ctoi(*input_pointer);
+        while(keycode != END) {
+            //do processing stuff
+            if(keycode == TERMINATE) {
+                //terminates the loop as the program should now end
+                running = 0;
+                break;
+            }
+
+            //TODO set up processor function
+            //update first unloaded and than loaded nodes
+            struct entll * curr_ent = unloaded;
+            while(curr_ent) {
+                //process cur_ent
+
+                cur_ent=cur_ent->next;
+            }
+            cur_ent = loaded;
+            while(cur_ent) {
+                //process cur_ent
+
+                cur_ent = cur_ent->next;
+            }
+
+            //set keycode to next keycode
+            keycode = ctoi(++input_pointer);
+        }
+
+        //TODO update shared memory list
+
+    }
+
+    //program has terminated, just need to clean up
+    close(fd[0]), fd[0] = NULL;
+    close(fd[1]), fd[1] = NULL;
+
+    destroy_entll_children(unloaded);
+    destroy_entll_children(loaded);
 
     return 0;
 }
